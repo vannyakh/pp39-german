0c75a7f2237340d2c7fa46614dcee7d6225a6106
KHUT lihong <khut.lihong@gmail.com>
2026-01-28 00:14:10 +0700

    Updates websocket and API URLs; clarifies password label
    Replaces hardcoded IP addresses with domain-based URLs for websocket and API connections to improve reliability and maintainability. Updates UI label for password confirmation to "Repeat Password" for better clarity in the login interface.

--- env-dev.js
+++ env-dev.js
@@ -7,9 +7,9 @@ export default {
 	VUE_APP_VERSION_API_URL: '',
 	VUE_APP_OSS_API_URL: 'https://nealika.sgp1.cdn.digitaloceanspaces.com/api-rolling-pp-39.txt', // testing
 	// VUE_APP_OSS_API_URL:'https://bv23real-api-rolling.s3.ca-central-1.amazonaws.com/api-rolling.txt', //real
-	VUE_APP_WSS_URL: 'wss://49.51.142.55:6688',
-	VUE_APP_WS_FOREX_URL: 'wss://49.51.142.55:2346',
-	VUE_APP_FOREX_KLINE_URL: "https://app.galaxyenv.com",
+	VUE_APP_WSS_URL: 'wss://wss.galaxyenv.com:6688',
+	VUE_APP_WS_FOREX_URL: 'wss://wss.galaxyenv.com:2346',
+	VUE_APP_FOREX_KLINE_URL: "https://kline.galaxyenv.com",
 	VUE_APP_LANG: 'en',
 	VUE_APP_VERSION: '1.0.0',
 
